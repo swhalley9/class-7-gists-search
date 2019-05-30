1a743dfa11762020db7c3158138c97b1a1f2c781
Stuart Whalley <50208733+swhalley9@users.noreply.github.com>
2019-05-29 23:29:36 -0600

    Update search.py
    Worked through the solution with class b34

--- gist_search/search.py
+++ gist_search/search.py
@@ -5,3 +5,43 @@ def search_gists(username, description=None, file_name=None):
     if not description and not file_name:
         print("At least one search parameter must be specified")
         return
+    
+    results = []
+    
+    gists = get_gists(username)
+    
+    for gist in gists:
+        if description and description.lower() not in gist['description'].lower():
+            continue
+            
+        if file_name:
+            found = False
+            
+            for fname in gist['files']:
+                if file_name.lower() in fname.lower:
+                    found = True
+                    break
+            if not found:
+                continue
+            
+        results.append(gist)
+        
+        
+    return results
+#         if description and file_name:
+            
+#         elif description:
+            
+#         elif file_name:
+        
+#         if description:
+#             if description.lower() in gist['description'].lower():
+#                 results.append(gist)
+                
+#         if file_name:
+#             for fname in gist['files']:
+#                 if file_name.lower() in fname.lower():
+#                     if gist not in results:
+#                         results.append(gist)
+#                     break
+
